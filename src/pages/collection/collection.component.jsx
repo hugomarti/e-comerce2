bdc18e0de25ca06d94d2e006bb5b8e7526fdb1c7
hugomarti <hgmarti@gmail.com>
2020-07-21 19:28:01 +0800

    implementing fuctionality rendering each collection from the shop url

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { selectCollection } from "../../redux/shop/shop.selectors";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import "./collection.styles.scss";
+
+const CollectionPage = ({ collection }) => (
+  <div className="collection">COLLECTION PAGE</div>
+);
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
