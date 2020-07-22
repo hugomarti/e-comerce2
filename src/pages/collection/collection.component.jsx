f218006b2e28ef23a7171fa9d011f3995cbc9203
hugomarti <hgmarti@gmail.com>
2020-07-22 11:37:27 +0800

    inplementing strype payments in the app, minor style updated

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -7,9 +7,17 @@ import CollectionItem from "../../components/collection-item/collection-item.com
 
 import "./collection.styles.scss";
 
-const CollectionPage = ({ collection }) => (
-  <div className="collection">COLLECTION PAGE</div>
+const CollectionPage = ({ collection: { title, items } }) => (
+  <div className="collection-page">
+    <h2 className="title">{title}</h2>
+    <div className="items">
+      {items.map((item) => (
+        <CollectionItem key={item.id} item={item} />
+      ))}
+    </div>
+  </div>
 );
+
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state),
 });
